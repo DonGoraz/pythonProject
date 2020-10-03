1a65a82347dbea0e214df2f083986c39f920d7c7
DonGoraz <63724668+DonGoraz@users.noreply.github.com>
2020-10-03 19:59:04 +0300

    add another decoration function func() example

--- decoratorius/exmpl.py
+++ decoratorius/exmpl.py
@@ -14,3 +14,28 @@ def div(a, b):
 # div = smart_div(div)
 
 print(div(10, 0))
+
+
+print("-" * 50)
+
+def func(f):
+    def wrapper():
+        print("Started")
+        f()
+        print("Ended")
+
+    return wrapper
+
+
+@func
+def func2():
+    print("I am func2")
+
+
+@func
+def func3():
+    print("I am func3")
+
+
+func3()
+func2()
